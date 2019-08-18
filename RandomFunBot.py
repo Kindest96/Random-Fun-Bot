0ccba2fda465a32a1f275fa67489f017f7643e30
Asif Nawaz <asif_nawaz96@live.com>
2019-08-18 19:53:50 +0500

    Initial Release

--- RandomFunBot.py
+++ RandomFunBot.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
-import logging
 import telebot as telegram
 import yaml
 
 def main():
     
-    with open(configpath) as f:
+    with open(configpath, "r") as f:
         config = yaml.safe_load(f)
     
     
-    logging.info(config)
+    print(config)
 
 if __name__ == "__main__":
     
